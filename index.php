307749093e87a7a130b667f9e170875701ca552b
lapiscine <alexandre.efstathiou@lapiscine.pro>
2018-03-02 16:14:20 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/custom/style.css">
+    <title>Exo GitHub</title>
+</head>
+<body>
+
+<div class="container">
+
+    <h1> Exercice GitHub </h1>
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci aliquam architecto corporis cumque, ea et
+        excepturi necessitatibus nihil nulla officia placeat quaerat quo rem repellat sequi, veniam voluptatem voluptatibus.
+    </div>
+    <div>Amet ducimus explicabo facere fugit officia quod saepe? A adipisci cum fugiat laudantium numquam quas similique
+        veniam. Autem, consequatur cumque error incidunt libero magnam nesciunt, nihil possimus praesentium qui quod.
+    </div>
+    <div>Aliquid assumenda consequatur consequuntur cumque deleniti distinctio est expedita illum labore laboriosam laborum,
+        minus perferendis, qui quidem quos repudiandae sequi, vitae voluptatum. Eaque eius facere, facilis libero minima
+        possimus sit?
+    </div>
+</div>
+</body>
+</html>
